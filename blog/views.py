3ea655b2b68f7f647d0da56d2a9e5a00ad2cf41e
mainuzzamanbabu <web4@daffodilvarsity.edu.bd>
2025-06-19 17:32:05 +0600

    Did the full cloning and solving issues of infinity job site

--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,5 @@ from .models import Post
 
 def home(request):
     posts = Post.objects.filter(published=True)
+    print("Did the full cloning and solving issues of infinity job site")
     return render(request, 'blog/home.html', {'posts': posts})
