75868ce0092fa16aac21caf941032d7dd4d401a7
mainuzzamanbabu <web4@daffodilvarsity.edu.bd>
2025-06-19 17:32:58 +0600

    Attended a long meeting with the team to discuss project UI and UX

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,5 @@ from .models import Post
 def home(request):
     posts = Post.objects.filter(published=True)
     print("Did the full cloning and solving issues of infinity job site")
+    print("Attended a long meeting with the team to discuss project UI and UX")
     return render(request, 'blog/home.html', {'posts': posts})
